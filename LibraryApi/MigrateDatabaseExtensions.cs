5829807880018fcd9b14b152dabb77f3501207d6
Bogge31 <34136797+Bogge31@users.noreply.github.com>
2020-06-03 20:10:02 +0000

    End of class checkin

---
+++ LibraryApi/MigrateDatabaseExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LibraryApi
+{
+    public static class MigrateDatabaseExtensions
+    {
+        public static IHost MigrateDatabase<T>(this IHost webHost) where T : DbContext
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                T db = null;
+                var services = scope.ServiceProvider;
+                try
+                {
+                    db = services.GetRequiredService<T>();
+                    db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred trying to migrate the database");
+                    try
+                    {
+                        Thread.Sleep(5000);
+                        db.Database.Migrate();
+                    }
+                    catch (Exception ex2)
+                    {
+                        logger.LogError(ex2, "Even waiting 5 seconds didn't help to be able to migrate");
+                    }
+                }
+            }
+
+            return webHost;
+        }
+    }
+}
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -18,6 +18,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitMqUtils;
 
 namespace LibraryApi
 {
@@ -33,6 +34,8 @@ namespace LibraryApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRabbit(Configuration);
+            services.AddScoped<ISendReservationsToTheQueue, RabbitMqReservationProcessor>();
             services.AddControllers()
                 .AddJsonOptions(options =>
                 {
